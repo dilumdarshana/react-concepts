2c9c82cf3968e2bf1a7a830f3b296ccbbbe73fd6
dilumdarshana <dilum.dar@gmail.com>
2025-02-03 18:15:05 +0530

    Tanstack

---
+++ tanstack/README.md
@@ -0,0 +1,10 @@
+# React Advanced concepts
+
+## The list
+- tanstack query
+
+## How to setup
+```bash
+# add react query to the project
+pnpm add @tanstack/react-query
+```
---
+++ tanstack/src/App.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+
+interface ResponseType {
+  title: string;
+}
+
+function App() {
+  const { data, isLoading, isPending, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: getTodos,
+  });
+
+  if (error) {
+    return <p>Error on fetching data</p>;
+  }
+
+  return (
+    <div className="container">
+      <ul>
+        {
+          data.map((item: ResponseType) => {
+            <li>
+              {item.title}
+            </li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+const getTodos = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  return response.json();
+};
+
+export default App;
