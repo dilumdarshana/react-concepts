b89df1594e6aab63701a0317fa9adc4e5ba28ad9
dilumdarshana <dilum.dar@gmail.com>
2025-02-04 12:27:46 +0530

    feat: boilerplate added

---
+++ boilerplate/src/layouts/Default.tsx
@@ -0,0 +1,38 @@
+import { Outlet } from 'react-router';
+import LeftMenu from '../components/LeftMenu';
+
+const Default = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Header */}
+      <header className="bg-blue-600 text-white p-4">
+        <div className="container mx-auto">
+          <h1 className="text-2xl font-bold">Simple Boilerplate</h1>
+        </div>
+      </header>
+
+      {/* Main content */}
+      <main className="flex-grow container mx-auto pt-4">
+        <div className="flex">
+          {/* Left Menu */}
+          <div className="w-64 bg-gray-100 p-4 border-r border-gray-200">
+            <LeftMenu />
+          </div>
+          {/* Main Content */}
+          <div className="flex-grow bg-white p-4">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-gray-800 text-white p-4 mt-auto">
+        <div className="container mx-auto">
+          <p className="text-center">&copy; 2025 xxxx.com. All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Default;
--- boilerplate/src/main.tsx
+++ boilerplate/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import Default from './layouts/Default';
+import About from './pages/About';
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route element={<Default />} >
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
