4aac36a348d9af2dc655756fdad2ad7f895c93a1
dilumdarshana <dilum.dar@gmail.com>
2025-09-09 19:02:19 +0530

    chore: remove unwanted codes from main.tsx

--- tanstack/src/main.tsx
+++ tanstack/src/main.tsx
@@ -1,46 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router';
-import UseQuery from './pages/UseQuery';
-import UseSuspenseQuery from './pages/UseSuspenseQuery';
-import Layout from './layouts/Layout';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 import './index.css';
-import UseQueryWIthParams from './pages/UseQueryWIthParams';
-import UseQueriesAsync from './pages/UseQueriesAsync';
-import UseQueriesSync from './pages/UseQueriesSync';
-import TanStackTable from './pages/TanStackTable';
-import TansStackTableDynamic from './pages/TanStackTableDynamic';;
-
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
 
 const queryClient = new QueryClient();
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <QueryClientProvider client={queryClient}>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route element={<Layout />} >
-//             <Route path='/' element={<UseQuery />} />
-//             <Route path='/use-suspense' element={<UseSuspenseQuery />} />
-//             <Route path='/use-query-multiple-async' element={<UseQueriesAsync />} />
-//             <Route path='/use-query-multiple-sync' element={<UseQueriesSync />} />
-//             <Route path='/usequery-params' element={<UseQueryWIthParams />} />
-//             <Route path='/tanstack-table' element={<TanStackTable />} />
-//             <Route path='/tanstack-table-dynamic' element={<TansStackTableDynamic />} />
-//           </Route>
-//         </Routes>
-//       </BrowserRouter>
-//     </QueryClientProvider>
-//   </StrictMode>,
-// )
-
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
-
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -50,7 +19,7 @@ declare module '@tanstack/react-router' {
 }
 
 // Render the app
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
   const root = createRoot(rootElement)
   root.render(
@@ -61,4 +30,3 @@ if (!rootElement.innerHTML) {
     </StrictMode>,
   )
 }
-
